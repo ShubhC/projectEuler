98e334fb0b34d2a99faebd94982d7a92ca9d8fc4
Chhabra <schhabra@amazon.com>
2017-12-27 03:12:53 +0530

    p2 Even Fibonacci numbers

---
+++ p2.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int sum = 0;
+    int f1 = 1;
+    int f2 = 1;
+    int lim = 4000000;
+    for(;(f1+f2)<=lim;) {
+        int f3 = f1+f2;
+        f1 = f2;
+        f2 = f3;
+        if( f3%2==0 ) {
+            sum += f3;
+        } 
+    }
+    cout << sum << endl;
+    return 0;
+}
