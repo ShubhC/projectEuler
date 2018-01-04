10761e4a52fdf25bb9930c2c5b37e1a2f9e94420
Chhabra <schhabra@amazon.com>
2018-01-04 23:06:40 +0530

    changing p9 implementation

--- p9.cpp
+++ p9.cpp
@@ -1,11 +1,10 @@
 #include "/Users/schhabra/Documents/codes/cppCodes/bits/stdc++.h"
 using namespace std;
 int main() {
-    for(int p=1;p<=998;++p) {
-        for(int q=p+1;q<1000;++q) {
-            long long int r = 1000-p-q;
-            if( r*r==(p*p+q*q) ) {
-                cout << p*q*r << endl;
+    for(int x=1;x<23;++x) {
+        for(int y=1;y<23;++y) {
+            if( x*(x+y)==500 ) {
+                cout << (x*x-y*y)*(x*x+y*y)*(2*x*y) << endl;
             }
         }
     }
